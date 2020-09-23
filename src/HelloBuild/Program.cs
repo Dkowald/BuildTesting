d0f422d52a9a613dab7779877fd9db8fdf9e878b
dkowald <derek@kowald.id.au>
2020-09-24 08:42:12 +1000

    Report version info in console app.

---
+++ src/HelloBuild/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace HelloBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello AppVeyor");
+            Console.WriteLine($"Assem Version: {AssemblyVersion}");
+            Console.WriteLine($"File Version: {FileVersion}");
+            Console.WriteLine($"Product Version: {ProductVersion}");
+
+            Console.WriteLine("Press key to exit");
+            Console.ReadKey();
+        }
+
+        private static string AssemblyVersion
+            => Assembly.GetExecutingAssembly()
+                    .GetName().Version.ToString();
+
+        private static string FileVersion =>
+            FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion; 
+
+        private static string ProductVersion =>
+            FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+    }
+}
